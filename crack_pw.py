bb4020bb3475ad75a88077205ef6b97cef492b74
Kalpak Take <kalpaktake@gmail.com>
2017-07-03 20:34:30 +0530

    Update case for file IOError and Fix bit ui

--- crack_pw.py
+++ crack_pw.py
@@ -7,15 +7,21 @@ filename = ""
 try:
   filename = sys.argv[1];
 except:
-  print("The filename was not a valid string")
-  exit(1)
+  print("The filename is not a valid string!")
+  sys.exit(1)
   
-#characters = string.ascii_letters
+#characters = string.ascii_letters 
 characters = string.ascii_lowercase
-zipFile = zipfile.ZipFile(filename, "r")
+try:
+  zipFile = zipfile.ZipFile(filename, "r")
+except IOError:
+  # rather than raise() we'll simple print it to screen because we dont't want to scare the user
+  print("IOError: \nFile you entered was not found!\nTry again!")     
+  sys.exit(1)
 
+found = False
 #iterate all possible lengths of the password
-for leng in range(1, 5):
+for leng in range(1, len(characters)+1):
   print("Length of password: ", leng)
 
   #create an iterator over the cartesian product of all possible permuations
@@ -23,28 +29,28 @@ for leng in range(1, 5):
 
   #iterate all created permutations
   for passw in opts:
+    if found:
+      sys.exit(0)
+    else:
       tmp = ''.join(passw)
       print("Trying with password {} :".format(tmp))
       try:
         #join the tuple to a string and set the password
         passwd = ''.join(passw)
-        zipFile.setpassword(passw)
+        zipFile.setpassword(passwd)
 
         """ 
-        try to extract the files from the file
-        if the password is wrong, an exception is thrown,
-        (RuntimeError), which is caught in the except part
+          try to extract the files from the file
+          if the password is wrong, an exception is thrown,
+            (RuntimeError), which is caught in the except part
         """
         zipFile.extractall()
 
-        #if there was no error the password will be shown and the programm exits
-        print("The password for file is: " + str(passw))
-        exit()
+        print("Password Found\n  Password is ->" + str(passwd))
+        found = True
       except RuntimeError:
-        print("Failed For {}\n".format(passw))
-      except zipfile.BadZipfile:
-        print("Error opening/operating zipfile\n")
+        print("Failed for test{}\n".format(tmp))
       except:
-        pass
+        print("Error opening/operating zipfile\n")
 
 print("No passwords were found...\n")
